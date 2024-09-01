29baad1b4c2fbb08c84c6cbd7d261609710cca6b
Eloisa <165907871+Eloisa-Bolis@users.noreply.github.com>
2024-09-01 15:40:17 -0300

    Add files via upload

---
+++ TorreHanoi/Movimento.java
@@ -0,0 +1,13 @@
+public class Movimento {
+    
+    int disco;
+    char origem, destino;
+
+    Movimento(int disco, char origem, char destino) {
+        this.disco = disco;
+        this.origem = origem;
+        this.destino = destino;
+        
+    }
+}
+//o codigo move um disco de uma torre para outra
---
+++ TorreHanoi/Resolve.java
@@ -0,0 +1,10 @@
+import java.util.Stack;
+
+public class Resolve {
+
+    public static void resolve(int d, char origem, char destino) {
+        Stack<Movimento> pilha = new Stack<>();
+        
+    }
+    
+}
